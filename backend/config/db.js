6d886f35e3c20bec91add86d8395c459d4bfd93f
Ravi Ranjan <2140131@sliet.ac.in>
2024-01-06 16:29:38 +0530

    refresh token updated

--- backend/config/db.js
+++ backend/config/db.js
@@ -6,7 +6,7 @@ dotenv.config();
 mongoose.set("strictQuery", true);
 const connectDB = () => {
   mongoose
-    .connect(process.env.MONGO_URI, {user: process.env.MONGO_USER, pass: process.env.MONGO_PWD, dbName: "techfest"})
+    .connect(process.env.MONGO_URI)
     .then((res) => console.log("Connected to MongoDB".bgBlue));
 };
 
